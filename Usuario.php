8f83c32182052a1f9d12714d351c0dba79d09ff0
Pauk <56691890+pauca881@users.noreply.github.com>
2025-03-02 21:38:55 +0100

    Model Zend1

---
+++ Usuario.php
@@ -0,0 +1,64 @@
+<?php
+require_once 'Zend/Db/Table/Abstract.php';
+
+
+class Application_Model_Usuario extends Zend_Db_Table_Abstract
+{
+
+
+    // public function __construct()
+    // {
+    //     $this->_dbTable = new Zend_Db_Table('usuarios');
+    // }
+
+    //Nombre de la tabla
+    protected $_name = 'usuarios';
+
+    //Clave primaria de la tabla
+    protected $_primary = 'id';
+    
+    public function getUsuarios()
+    {
+        return $this->fetchAll();
+        //return $this->_dbTable->fetchAll();
+
+    }
+    
+    public function getUsuario($id)
+    {
+        $id = (int)$id;
+        return $this->find($id)->current();
+    }
+    
+    public function addUsuario($data)
+    {
+        $data = array(
+            'nombre' => $data['nombre'],
+            'email' => $data['email'],
+            // La fecha_creacion se genera automáticamente si configuraste CURRENT_TIMESTAMP
+        );
+        
+        return $this->insert($data);
+    }
+    
+    public function updateUsuario($id, $data)
+    {
+        $id = (int)$id;
+        $data = array(
+            'nombre' => $data['nombre'],
+            'email' => $data['email']
+        );
+        
+        $where = $this->getAdapter()->quoteInto('id = ?', $id);
+        return $this->update($data, $where);
+    }
+    
+    public function deleteUsuario($id)
+    {
+        $id = (int)$id;
+        $where = $this->getAdapter()->quoteInto('id = ?', $id);
+        return $this->delete($where);
+    }
+
+}
+
